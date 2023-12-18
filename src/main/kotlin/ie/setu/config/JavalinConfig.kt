1df39c5b1b57188b05b23fd638cf90a315be1f3c
Javid Moradi <javid.moradi@ug.bilkent.edu.tr>
2023-12-18 22:17:22 +0300

    Add Activity and related classes

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -1,17 +1,25 @@
 package ie.setu.config
 
+import com.fasterxml.jackson.module.kotlin.jsonMapper
+import ie.setu.controllers.ActivityController
 import ie.setu.controllers.HealthTrackerController
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.json.JavalinJackson
 
 class JavalinConfig
 {
     fun startJavalinService(): Javalin
     {
-        val app = Javalin.create().apply {
-            exception(Exception::class.java) { e, _ -> e.printStackTrace() }
-            error(404) { ctx -> ctx.json("404 : Not Found") }
-        }.start(getRemoteAssignedPort())
+        val app = Javalin.create {
+            //add this jsonMapper to serialise objects to json
+            it.jsonMapper(JavalinJackson(jsonMapper()))
+        }
+            .apply {
+                exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+                error(404) { ctx -> ctx.json("404 - Not Found") }
+            }
+            .start(getRemoteAssignedPort())
 
         registerRoutes(app)
         return app
@@ -27,10 +35,17 @@ class JavalinConfig
                     get(HealthTrackerController::getUserByUserId)
                     delete(HealthTrackerController::deleteUser)
                     patch(HealthTrackerController::updateUser)
+                    path("activities") {
+                        get(ActivityController::getActivitiesByUserId)
+                    }
                 }
                 path("email/{email}") {
                     get(HealthTrackerController::getUserByEmail)
                 }
+            }
+            path("/api/activities") {
+                get(ActivityController::getAllActivities)
+                post(ActivityController::addActivity)
             }
         }
     }
